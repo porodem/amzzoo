878cab9a1aedf586e103691765697e747b6d6ddc
Anatolii D <porodem@gmail.com>
2024-08-25 13:49:59 +0700

    database buttons and check player

--- amzoo.py
+++ amzoo.py
@@ -2,7 +2,8 @@
 
 
 import telebot
-
+import re
+import sql_helper
 # https://pypi.org/project/emoji/
 import emoji
 
@@ -23,10 +24,68 @@ bot = telebot.TeleBot(token, parse_mode=None)
 
 
 
-
+c_start = types.BotCommand('start','Начать')
 my_pets_command = types.BotCommand('show_pets','Показать питомцев')
-bc_a = types.BotCommand('fun','anekdot')
-bot.set_my_commands([my_pets_command,bc_a])
+main_menu = types.BotCommand('main_menu','Меню')
+main_menu = types.BotCommand('earn_money','Найти деньги')
+bot.set_my_commands([my_pets_command,main_menu])
+
+# buttons test
+@bot.message_handler(commands=['start'])
+def show_pets(message):
+
+    # check if user exists
+    if sql_helper.db_check_player_exists(message.from_user.id) is None:
+        msg_json = message.json
+        print(msg_json)
+        # new_member = msg_json["new_chat_member"]
+        # tid = new_member["id"]
+        # print(new_member)
+        # vname = new_member["username"]
+
+        sql_helper.db_new_player(message.from_user.id,message.from_user.username,'x')
+
+    else:
+
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("🐇 Питомцы")
+        btn2 = types.KeyboardButton("🏫 Имущество")
+        btn3 = types.KeyboardButton("⚒ Работы")
+        markup.add(btn1,btn2,btn3)
+        bot.send_message(message.from_user.id, "select :", reply_markup=markup)  
+        bot.register_next_step_handler(message, search_money)
+
+# buttons test
+@bot.message_handler(commands=['show_pets'])
+def show_pets(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("🐣 my pet")
+    btn2 = types.KeyboardButton("😀 test")
+    markup.add(btn1,btn2)
+    bot.send_message(message.from_user.id, "select :", reply_markup=markup)  
+    bot.register_next_step_handler(message, step_two)
+
+# buttons test
+@bot.message_handler(commands=['earn_money'])
+def show_pets(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("⛏ Искать клад")
+    btn2 = types.KeyboardButton("⚒ Работать")
+    markup.add(btn1,btn2)
+    bot.send_message(message.from_user.id, "select :", reply_markup=markup)  
+    bot.register_next_step_handler(message, search_money)
+
+def search_money(message):
+    if re.match('.*клад.*',message.text):
+        bot.reply_to(message, 'pet option')
+    else:
+        bot.reply_to(message, 'test option')
+
+def step_two(message):
+    if re.match('.*pet.*',message.text):
+        bot.reply_to(message, 'pet option')
+    else:
+        bot.reply_to(message, 'test option')
 
 # legacy func from previous project
 @bot.message_handler(regexp=".*(pet|пет).*")
@@ -35,12 +94,25 @@ def attach_ls(message):
     markup.add(types.KeyboardButton('show pets'))
     msg = bot.send_message(message.chat.id, "choose type shit", reply_markup=markup)
 
+    
+
 #anything other messages
 @bot.message_handler(func=lambda m:True)
 def echo_all(message):
     print('---------- ANYTHING -----------')
-    bot.reply_to(message,emoji.emojize('Python is :thumbs_up:'))
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("🐇 Питомцы")
+    btn2 = types.KeyboardButton("🏫 Имущество")
+    btn3 = types.KeyboardButton("⚒ Работы")
+    btn4 = types.KeyboardButton(" test")
+    markup.add(btn1,btn2,btn3,btn4)
+    bot.send_message(message.from_user.id, "select :", reply_markup=markup)  
+    bot.register_next_step_handler(message, next_option)
 
 bot.infinity_polling()
 
-
+def next_option(message):
+    if re.match('.*pet.*',message.text):
+        bot.reply_to(message, 'pet option')
+    else:
+        bot.reply_to(message, 'test option')
---
+++ sql_helper.py
@@ -0,0 +1,34 @@
+# test postgres for AMZOO telegram bot
+
+import psycopg
+from datetime import datetime, timedelta
+
+con = psycopg.connect('dbname=amzoo user=pet_master host=localhost password=dashakuromi')
+
+def db_check_player_exists(tid):
+    print('- - - check exist player - - -')
+    q = '''SELECT telegram_id from players where telegram_id = %s'''
+    cur = con.cursor()
+    cur.execute(q,(tid,))
+    b = cur.fetchone()
+    if b is None:
+        result = None
+    else:
+        print(b[0])
+        result = b[0]
+
+    return result
+
+
+
+def db_new_player(tid,username,nickname):
+    print(' - - write new user to DB - -')
+    q = '''INSERT INTO players(telegram_id, invite_date, username, nick_name, coins) VALUES (%s, current_date, %s,%s, 0);'''
+    cur = con.cursor()
+    cur.execute(q,(tid,username,nickname))
+    con.commit()
+
+def db_add_money(tid, value):
+    print('- - - write money to DB - - - ')
+    q = '''UPDATE players set coins = %s where telegram_id = %s;'''
+
